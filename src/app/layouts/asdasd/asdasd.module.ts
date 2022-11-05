e4d1811f6b92ade27f0a354bb438a0267ba73780
Samurai2303 <meleschenko.andriy@gmail.com>
2022-11-05 12:51:50 +0200

    Lesson 5 in progress

---
+++ src/app/layouts/asdasd/asdasd.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AsdasdRoutingModule } from './asdasd-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AsdasdRoutingModule
+  ]
+})
+export class AsdasdModule { }
